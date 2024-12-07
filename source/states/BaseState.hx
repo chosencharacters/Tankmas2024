ded0a41c981e52a0f46c218f3746c6d5ab544c51
Dungeonation <75049814+Dungeonation@users.noreply.github.com>
2024-12-06 22:37:51 -0500

    Update BaseState.hx

--- source/states/BaseState.hx
+++ source/states/BaseState.hx
@@ -31,6 +31,7 @@ class BaseState extends FlxState
 			#if !no_radio
 			radio = new RadioManager();
 			#else
+			if(Main.time.day < 7) SoundPlayer.music('thewanderer-everratic');
 			if(Main.time.day > 6) SoundPlayer.music('dancinginthesnow-twistedfw');
 			#end
 
