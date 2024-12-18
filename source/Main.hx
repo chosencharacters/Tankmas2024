bb252179108cb8a5bc901097e9b3c4ae042237fb
chosencharacters <chosencharacter@gmail.com>
2024-12-18 07:54:19 -0500

    day 18

--- source/Main.hx
+++ source/Main.hx
@@ -35,7 +35,7 @@ class Main extends Sprite
 
 	public static var default_costume_collection:Array<String> = ["tankman", "paco"];
 	public static var default_costume:String = "tankman";
-	public static var default_pet:String = "";
+	public static var default_pet:String = "invisible-pet";
 
 	public static var default_room:String = "outside_hotel";
 
