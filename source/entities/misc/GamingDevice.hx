90fc551ddb5a52ef2c6871a4143c46ec166b6a52
chosencharacters <chosencharacter@gmail.com>
2024-12-19 09:06:09 -0500

    day 19 + xml re-org

--- source/entities/misc/GamingDevice.hx
+++ source/entities/misc/GamingDevice.hx
@@ -1,22 +1,27 @@
 package entities.misc;
 
+import data.SaveManager;
 import video.VideoSubstate;
 
 class GamingDevice extends Interactable
 {
 	var url:String = "https://uploads.ungrounded.net/tmp/6257000/6257910/file/alternate/alternate_1.720p.mp4?f1733891286";
+	// TEst video
+	// var url = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
+	var video_overlay:VideoSubstate;
+
+	var sound_played:Bool = false;
 
 	public function new(?X:Float, ?Y:Float)
 	{
 		super(X, Y);
 
-		PlayState.self.props_foreground.add(this);
+		PlayState.self.world_objects.add(this);
 
 		loadAllFromAnimationSet("gaming-device");
 		sstate(IDLE);
-		
-		detect_range = 300;
 
+		detect_range = 300;
 
 		interactable = true;
 	}
@@ -35,14 +40,19 @@ class GamingDevice extends Interactable
 				anim("idle");
 			case NEARBY:
 				animProtect("nearby");
-				if (Ctrl.mode.can_move && (Ctrl.jinteract[1] || FlxG.mouse.overlaps(this) && FlxG.mouse.justReleased))
-					start_video();
+				if (!sound_played)
+					SoundPlayer.sound("gaming-device-near", 0.5);
 		}
+
+	override public function on_interact()
+	{
+		super.on_interact();
+		start_video();
+	}
 
 	override public function mark_target(mark:Bool)
 	{
-		trace("!!!")
-;		if (mark && interactable)
+		if (mark && interactable)
 			sstate(NEARBY);
 		if (!mark && interactable)
 			sstate(IDLE);
@@ -50,12 +60,21 @@ class GamingDevice extends Interactable
 
 	function start_video()
 	{
-		PlayState.self.openSubState(new VideoSubstate(url));
+		if (video_overlay != null)
+			return;
+		video_overlay = new VideoSubstate(url);
+		PlayState.self.openSubState(video_overlay);
+		video_overlay.on_close = () ->
+		{
+			Flags.set_bool("MELLA_PRESENT_UNLOCKED", true);
+			SaveManager.save();
+			video_overlay = null;
+		}
 	}
 
 	override function kill()
 	{
-		PlayState.self.props_foreground.remove(this, true);
+		PlayState.self.world_objects.remove(this, true);
 		super.kill();
 	}
 }
